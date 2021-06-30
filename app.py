96a2d9f3368987cc2f070e30c0b0ca2a5d354fb2
Kelley Lynch <kelleylynch@gmail.com>
2021-06-30 12:50:21 -0400

    more updates for clams-python v 0.4.0

--- app.py
+++ app.py
@@ -1,5 +1,4 @@
-from clams.app import ClamsApp
-from clams.restify import Restifier
+from clams import ClamsApp, Restifier, AppMetadata
 
 from tesseract_utils import *
 
@@ -12,21 +11,29 @@ class OCR(ClamsApp):
             "name": "Tesseract OCR",
             "description": "This tool applies Tesseract OCR to an "
             "image and generates text boxes and OCR result.",
+            "app_version": APP_VERSION,
+            "license":"", # todo look up wrappee license
             "identifier": f"http://mmif.clams.ai/apps/tesseract/{APP_VERSION}",
             "input": [
-            { "@type": DocumentTypes.ImageDocument.value,
-            "required":False,
-            },
-            {"@type":DocumentTypes.VideoDocument.value,
-            "required":False} # todo are both of these really false?
+                {
+                    "@type": DocumentTypes.ImageDocument,
+                    "required": False,
+                },
+                {
+                    "@type": DocumentTypes.VideoDocument,
+                    "required": False,
+                },  # todo are both of these really false?
             ],
             "output": [
-                {"@type":AnnotationTypes.BoundingBox.value, "properties":{"frameType":"string"}},
-                {"@type":AnnotationTypes.Alignment.value},
-                {"@type":DocumentTypes.TextDocument.value},
+                {
+                    "@type": AnnotationTypes.BoundingBox,
+                    "properties": {"frameType": "string"},
+                },
+                {"@type": AnnotationTypes.Alignment},
+                {"@type": DocumentTypes.TextDocument},
             ],
         }
-        return metadata
+        return AppMetadata(**metadata)
 
     def _annotate(self, mmif_obj: Mmif, **kwargs) -> Mmif:
         """
@@ -36,9 +43,9 @@ class OCR(ClamsApp):
         """
         new_view = mmif_obj.new_view()
         config = self.get_configuration(**kwargs)
-        self.sign_view(v, config)
+        self.sign_view(new_view, config)
         new_view.new_contain(DocumentTypes.TextDocument)
-        box_type = kwargs.pop('boxType') if 'boxType' in kwargs else None
+        box_type = kwargs.pop("boxType") if "boxType" in kwargs else None
         if box_type:
             mmif_obj = box_ocr(mmif_obj, new_view, box_type, **kwargs)
         else:
@@ -50,3 +57,4 @@ if __name__ == "__main__":
     ocr_tool = OCR()
     ocr_service = Restifier(ocr_tool)
     ocr_service.run()
+s
