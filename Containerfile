f47a5cacaccf45095ec2b6c241493f484057a789
snewman-aa <snewman.aa@gmail.com>
2023-06-13 15:03:35 -0400

    updated to sdk-v1
    Copied sdk-v0.5 version of tesseract to new v1 app skeleton

--- Containerfile
+++ Containerfile
@@ -1,4 +1,17 @@
+<<<<<<< Updated upstream
 FROM ghcr.io/clamsproject/clams-python-opencv4:1.0.2
+=======
+# Use the same base image version as the clams-python python library version
+FROM ghcr.io/clamsproject/clams-python:1.0.2
+# See https://github.com/orgs/clamsproject/packages?tab=packages&q=clams-python for more base images
+# IF you want to automatically publish this image to the clamsproject organization, 
+# 1. you should have generated this template without --no-github-actions flag
+# 1. to add arm64 support, change relevant line in .github/workflows/container.yml 
+#     * NOTE that a lots of software doesn't install/compile or run on arm64 architecture out of the box 
+#     * make sure you locally test the compatibility of all software dependencies before using arm64 support 
+# 1. use a git tag to trigger the github action. You need to use git tag to properly set app version anyway
+
+>>>>>>> Stashed changes
 ################################################################################
 # DO NOT EDIT THIS SECTION
 ARG CLAMS_APP_VERSION
@@ -8,6 +21,7 @@ ENV CLAMS_APP_VERSION ${CLAMS_APP_VERSION}
 ################################################################################
 # clams-python base images are based on debian distro
 # install more system packages as needed using the apt manager
+<<<<<<< Updated upstream
 RUN apt-get update && apt-get install -y build-essential cmake \
     wget git unzip
 
@@ -53,6 +67,8 @@ RUN apt-get install -y \
 #        && \
 #    apt-get clean && \
 #    rm -rf /opencv /opencv_contrib /var/lib/apt/lists/*
+=======
+>>>>>>> Stashed changes
 ################################################################################
 
 ################################################################################
--- metadata.py
+++ metadata.py
@@ -9,6 +9,7 @@ from mmif import DocumentTypes, AnnotationTypes
 from clams.appmetadata import AppMetadata
 
 
+<<<<<<< Updated upstream
 # DO NOT CHANGE the function name 
 def appmetadata() -> AppMetadata:
     
@@ -20,6 +21,30 @@ def appmetadata() -> AppMetadata:
         url="https://github.com/clamsproject/app-tesseractocr-wrapper",
         analyzer_version='tesseract4',
         analyzer_license='apache',
+=======
+def appmetadata() -> AppMetadata:
+    """
+    Function to set app-metadata values and return it as an ``AppMetadata`` obj.
+    Read these documentations before changing the code below
+    - https://sdk.clams.ai/appmetadata.html metadata specification. 
+    - https://sdk.clams.ai/autodoc/clams.appmetadata.html python API
+    
+    :return: AppMetadata object holding all necessary information.
+    """
+    
+    metadata = AppMetadata(
+        name="Tesseract OCR Wrapper",
+        description="This tool applies Tesseract OCR to a video or"
+            "image and generates text boxes and OCR results.",
+        app_license="MIT",
+        identifier="tesseract",  # from 'tesseract' in the old id full url: http://apps.clams.ai/tesseract/...
+        url="https://github.com/clamsproject/app-tesseractocr-wrapper",
+        analyzer_version='version_4.0.0-2',   # https://packages.debian.org/buster/tesseract-ocr
+        # if pytesseract
+        # analyzer_version=[line.strip().rsplit('==')[-1]
+        #                  for line in open('requirements.txt').readlines() if re.match(r'^pytesseract==', line)][0],
+        analyzer_license="apache",
+>>>>>>> Stashed changes
     )
     metadata.add_input(DocumentTypes.VideoDocument)
     metadata.add_input(AnnotationTypes.BoundingBox, required=False, boxType='text')
@@ -27,10 +52,20 @@ def appmetadata() -> AppMetadata:
     metadata.add_output(DocumentTypes.TextDocument)
     metadata.add_output(AnnotationTypes.BoundingBox)
     metadata.add_output(AnnotationTypes.Alignment)
+<<<<<<< Updated upstream
     metadata.add_parameter(name='boxType', type='string',
                            description='When set, use exising "text"-typed ``BoundingBox`` annotations '
                                        'and run tesseract only on those regions, instead of entire frames.',
                            default=' ') # was this whitespace due to https://github.com/clamsproject/clams-python/issues/110 ?
+=======
+    
+    # (optional) and finally add runtime parameter specifications
+    metadata.add_parameter(name='boxType', type='string',
+                           description='When set, use exising "text"-typed ``BoundingBox`` annotations '
+                                       'and run tesseract only on those regions, instead of entire frames.',
+                           default=' ')
+
+>>>>>>> Stashed changes
     return metadata
 
 
