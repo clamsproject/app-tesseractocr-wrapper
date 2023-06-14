535bd4fd13474bd5eda56e46aa22643994d22f8b
Keigh Rim <keigh.rim@gmail.com>
2023-06-13 20:44:39 -0400

    Revert "updated to sdk-v1"
    This reverts commit f47a5cacaccf45095ec2b6c241493f484057a789.

--- Containerfile
+++ Containerfile
@@ -1,17 +1,4 @@
-<<<<<<< Updated upstream
 FROM ghcr.io/clamsproject/clams-python-opencv4:1.0.2
-=======
-# Use the same base image version as the clams-python python library version
-FROM ghcr.io/clamsproject/clams-python:1.0.2
-# See https://github.com/orgs/clamsproject/packages?tab=packages&q=clams-python for more base images
-# IF you want to automatically publish this image to the clamsproject organization, 
-# 1. you should have generated this template without --no-github-actions flag
-# 1. to add arm64 support, change relevant line in .github/workflows/container.yml 
-#     * NOTE that a lots of software doesn't install/compile or run on arm64 architecture out of the box 
-#     * make sure you locally test the compatibility of all software dependencies before using arm64 support 
-# 1. use a git tag to trigger the github action. You need to use git tag to properly set app version anyway
-
->>>>>>> Stashed changes
 ################################################################################
 # DO NOT EDIT THIS SECTION
 ARG CLAMS_APP_VERSION
@@ -21,7 +8,6 @@ ENV CLAMS_APP_VERSION ${CLAMS_APP_VERSION}
 ################################################################################
 # clams-python base images are based on debian distro
 # install more system packages as needed using the apt manager
-<<<<<<< Updated upstream
 RUN apt-get update && apt-get install -y build-essential cmake \
     wget git unzip
 
@@ -67,8 +53,6 @@ RUN apt-get install -y \
 #        && \
 #    apt-get clean && \
 #    rm -rf /opencv /opencv_contrib /var/lib/apt/lists/*
-=======
->>>>>>> Stashed changes
 ################################################################################
 
 ################################################################################
--- app.py
+++ app.py
@@ -1,53 +1,29 @@
 import argparse
-<<<<<<< Updated upstream
 
 from clams import ClamsApp, Restifier, AppMetadata
-=======
-from typing import Union
->>>>>>> Stashed changes
 
-# mostly likely you'll need these modules/classes
-from clams import ClamsApp, Restifier
-from mmif import Mmif, View, Annotation, Document, AnnotationTypes, DocumentTypes
 from tesseract_utils import *
 
-<<<<<<< Updated upstream
 
 class OCR(ClamsApp):
     def _appmetadata(self) -> AppMetadata:
         pass
 
     def _annotate(self, mmif_obj: Mmif, **kwargs) -> Mmif:
-=======
-
-class OCRWrapper(ClamsApp):
-
-    def __init__(self):
-        super().__init__()
-
-    def _appmetadata(self):
-        # see https://sdk.clams.ai/autodoc/clams.app.html#clams.app.ClamsApp._load_appmetadata
-        # Also check out ``metadata.py`` in this directory. 
-        # When using the ``metadata.py`` leave this do-nothing "pass" method here. 
-        pass
-
-    def _annotate(self, mmif: Union[str, dict, Mmif], **runtime_params) -> Mmif:
-        # see https://sdk.clams.ai/autodoc/clams.app.html#clams.app.ClamsApp._annotate
->>>>>>> Stashed changes
         """
         :param mmif_obj: this mmif could contain images or video, with or without preannotated text boxes
         :param **kwargs:
         :return: annotated mmif as string
         """
-        new_view = mmif.new_view()
-        self.sign_view(new_view, self.get_configuration(**runtime_params))
+        new_view = mmif_obj.new_view()
+        self.sign_view(new_view, self.get_configuration(**kwargs))
         new_view.new_contain(DocumentTypes.TextDocument)
-        box_type = runtime_params.pop("boxType").strip() if "boxType" in runtime_params else None
+        box_type = kwargs.pop("boxType").strip() if "boxType" in kwargs else None
         if box_type:
-            mmif = box_ocr(mmif, new_view, box_type, **runtime_params)
+            mmif_obj = box_ocr(mmif_obj, new_view, box_type, **kwargs)
         else:
-            mmif = full_ocr(mmif, new_view, **runtime_params)
-        return mmif
+            mmif_obj = full_ocr(mmif_obj, new_view, **kwargs)
+        return mmif_obj
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
@@ -61,7 +37,6 @@ if __name__ == "__main__":
     parsed_args = parser.parse_args()
 
     # create the app instance
-<<<<<<< Updated upstream
     ocr_tool = OCR()
 
     ocr_service = Restifier(ocr_tool, port=int(parsed_args.port)
@@ -70,12 +45,3 @@ if __name__ == "__main__":
         ocr_service.serve_production()
     else:
         ocr_service.run()
-=======
-    app = OCRWrapper()
-
-    http_app = Restifier(app, port=int(parsed_args.port))
-    if parsed_args.production:
-        http_app.serve_production()
-    else:
-        http_app.run()
->>>>>>> Stashed changes
